8d416467989192d2bb8013d4e56554069eedc8d8
Corinne Marshall <corinnemarshall@Corinnes-MacBook-Air.local>
2021-10-27 15:03:28 -0400

    added seedAll function to seeds/index.js

---
+++ seeds/index.js
@@ -0,0 +1,19 @@
+const seedUsers = require('./user-seeds');
+const seedProducts = require('./product-seeds');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('--------------------');
+    
+    await seedUsers();
+    console.log('--------------------');
+
+    await seedProducts();
+    console.log('--------------------');
+
+    process.exit(0);
+};
+
+seedAll();
