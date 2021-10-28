e0592a3dccc64d8fb8806f8833a597aebb492384
Nick Giuliani <nicholas.giuliani94@gmail.com>
2021-10-27 21:34:44 -0400

    api/user-routes complete and working, tested with insomnia

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -99,6 +99,61 @@ router.post('/login', (req, res) => {
     });
 });
 
+// Logout Route
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    }
+    else {
+      res.status(404).end();
+    }
+  });
+
+// Update User data
+
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+          id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+// Delete User
+
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
 
 
 module.exports = router;
