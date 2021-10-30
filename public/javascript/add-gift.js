649511ba77d0d3184f9c5cb52101c2097b640592
Nick Giuliani <nicholas.giuliani94@gmail.com>
2021-10-30 19:07:19 -0400

    add-gift template added with working fetch request to create a new gift in the db

---
+++ public/javascript/add-gift.js
@@ -0,0 +1,27 @@
+async function addGiftHandler(event) {
+    event.preventDefault();
+    const recipient = document.querySelector('#recipient').value.trim();
+    const gift_name = document.querySelector('#gift-name').value.trim();
+    const occasion = document.querySelector('#occasion').value.trim();
+
+
+    if (recipient && gift_name && occasion) { 
+      const response = await fetch('/api/gifts', {
+        method: 'post',
+        body: JSON.stringify({
+          recipient,
+          gift_name,
+          occasion
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        alert('Gift Added!');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+
+document.querySelector('.create-gift-form').addEventListener('submit', addGiftHandler);
