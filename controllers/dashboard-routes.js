cd397297e2d45748257a5c1e43df3dbdba671ff1
Nick Giuliani <nicholas.giuliani94@gmail.com>
2021-10-30 02:14:14 -0400

    Get all gifts for user integrated with dashboard template

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -7,12 +7,12 @@ const withAuth = require('../utils/auth');
 
 // Get all Gift List products.  Need to add 'withAuth' as parameter. Left out for testing.
 
-router.get('/', (req, res) => {
+router.get('/', withAuth, (req, res) => {
     console.log(req.session);
     console.log('======================');
     Gift.findAll({
         where: {
-            user_id: req.session.id
+            user_id: req.session.user_id
         },
         attributes: [
             'id',
@@ -28,8 +28,9 @@ router.get('/', (req, res) => {
         ]
     })
     .then(dbGiftData => {
+        console.log(dbGiftData);
         const gifts = dbGiftData.map(gift => gift.get({ plain: true }));
-        res.render('dashboard', { gifts });
+        res.render('dashboard', { gifts, loggedIn: true });
     })
     .catch(err => {
         console.log(err);
