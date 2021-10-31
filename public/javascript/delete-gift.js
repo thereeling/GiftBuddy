13695cf5ecb74120f19cd205423f23637559f20a
Nick Giuliani <nicholas.giuliani94@gmail.com>
2021-10-31 16:45:19 -0400

    delete button works and deletes the gift from the db

---
+++ public/javascript/delete-gift.js
@@ -0,0 +1,20 @@
+const deleteBtns = document.getElementsByClassName('delete-button');
+
+for (let i = 0; i < deleteBtns.length; i++) {
+    deleteBtns[i].addEventListener('click', async function () {
+        let giftId = deleteBtns[i].id.replace(/-delete/g, '');
+        const response = await fetch(`/api/gifts/${giftId}`, {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          });
+          if(response.ok){
+              alert('Gift deleted!');
+              document.location.replace('/dashboard');
+          }
+          else{
+            alert(response.statusText);
+          }
+    })    
+};
