ff47516d2722e0d079e867c01d71a7c0be52edb3
Nick Giuliani <nicholas.giuliani94@gmail.com>
2021-10-31 15:43:26 -0400

    edit gift btn now does a get fetch from the server for one gift, then fills in the form with the data from the server.  Working on creating a 'Update Gift' button that will replace the 'add gift' button once the data is populated

--- public/javascript/edit-gift.js
+++ public/javascript/edit-gift.js
@@ -1,8 +1,45 @@
+var recipient = document.querySelector('#recipient');
+var gift_name = document.querySelector('#gift-name');
+var occasion = document.querySelector('#occasion');
+var editBtns = document.getElementsByClassName('edit-button');
 
-const editBtns = document.getElementsByClassName('edit-button');
 for (let i = 0; i < editBtns.length; i++) {
-    editBtns[i].addEventListener('click', function() {
+    editBtns[i].addEventListener('click', async function() {
        let giftId = editBtns[i].id.replace(/-edit/g, '');
        console.log(giftId);
-    });    
-};
+       const getResponse = await fetch(`/api/gifts/${giftId}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    
+      if (getResponse.ok) {
+        getResponse.json()
+        .then(getResponseData => {
+            recipient.value = getResponseData.recipient;
+            gift_name.value = getResponseData.gift_name;
+            occasion.value = getResponseData.occasion;
+        })
+      } else {
+        alert(getResponse.statusText);
+      }
+    //    const putResponse = await fetch(`/api/gifts/${giftId}`, {
+    //     method: 'PUT',
+    //     body: JSON.stringify({
+    //       recipient,
+    //       gift_name,
+    //       occasion
+    //     }),
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     }
+    //   });
+    
+    //   if (response.ok) {
+    //     document.location.replace('/dashboard/');
+    //   } else {
+    //     alert(response.statusText);
+    //   }
+    })
+    };  
