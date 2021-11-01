409be21b046ab1014749e538c9973c5b08056514
Corinne Marshall <corinnemarshall@Corinnes-MacBook-Air.local>
2021-11-01 15:21:53 -0400

    messing around with materialize

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -3,7 +3,7 @@ async function loginFormHandler(event) {
   
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
-    console.log('Hello');
+    
     if (email && password) { 
       const response = await fetch('/api/users/login', {
         method: 'post',
