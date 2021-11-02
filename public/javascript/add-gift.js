7e25da821f1660ff80249ef874b7a234f4d0cb46
Corinne Marshall <corinnemarshall@Corinnes-MacBook-Air.local>
2021-11-02 15:20:52 -0400

    added ability to dynamically add gifts to page

--- public/javascript/add-gift.js
+++ public/javascript/add-gift.js
@@ -1,27 +1,27 @@
 async function addGiftHandler(event) {
     event.preventDefault();
-    const recipient = document.querySelector('input[name="recipient"]').value.trim();
-    const gift_name = document.querySelector('input[name="gift-name"]').value.trim();
-    const occasion = document.querySelector('input[name="occasion"').value.trim();
 
+    const recipient = document.querySelector('#recipient').value.trim();
+    const gift_name = document.querySelector('#gift-name').value.trim();
+    const occasion = document.querySelector('#occasion').value.trim();
 
-    if (recipient && gift_name && occasion) { 
-      const response = await fetch('/api/gifts', {
-        method: 'POST',
-        body: JSON.stringify({
-          recipient,
-          gift_name,
-          occasion
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert(response.statusText);
+    const response = await fetch('/api/gifts', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipient,
+        gift_name,
+        occasion
+      }),
+      headers: { 
+        'Content-Type': 'application/json' 
       }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
     }
   }
 
-document.getElementById('.new-gift-form').addEventListener('click', addGiftHandler);
+document.querySelector('.new-gift-form').addEventListener('submit', addGiftHandler);
