16cbb3ed106d2945d81c8a650698ac746b42b1de
Corinne Marshall <corinnemarshall@Corinnes-iMac.local>
2021-11-02 20:17:51 -0400

    merging to develop

--- public/javascript/add-gift.js
+++ public/javascript/add-gift.js
@@ -1,27 +1,27 @@
 async function addGiftHandler(event) {
     event.preventDefault();
+
     const recipient = document.querySelector('#recipient').value.trim();
     const gift_name = document.querySelector('#gift-name').value.trim();
     const occasion = document.querySelector('#occasion').value.trim();
 
-
-    if (recipient && gift_name && occasion) { 
-      const response = await fetch('/api/gifts', {
-        method: 'post',
-        body: JSON.stringify({
-          recipient,
-          gift_name,
-          occasion
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        alert('Gift Added!');
-      } else {
-        alert(response.statusText);
+    const response = await fetch('/api/gifts', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipient,
+        gift_name,
+        occasion
+      }),
+      headers: { 
+        'Content-Type': 'application/json' 
       }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
     }
   }
 
-document.getElementById('gift-submit').addEventListener('click', addGiftHandler);
+document.querySelector('.new-gift-form').addEventListener('submit', addGiftHandler);
