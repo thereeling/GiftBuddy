1b3cbca810777b56eecec59b487b4cddf1859bac
Nick Giuliani <nicholas.giuliani94@gmail.com>
2021-11-05 14:15:10 -0400

    creating new branch for heroku fix

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,10 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         dialect: 'mysql',
     });
 
 module.exports = sequelize;
-
-
-// process.env.JAWSDB_URL
-//     ? new Sequelize(process.env.JAWSDB_URL)
