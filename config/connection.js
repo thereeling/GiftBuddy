655501db475004be01b140ee8d449f34807bdfad
Nick Giuliani <nicholas.giuliani94@gmail.com>
2021-11-05 01:59:15 -0400

    changed sequelize connection

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,12 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         dialect: 'mysql',
-        port: process.env.PORT
     });
 
 module.exports = sequelize;
+
+
+// process.env.JAWSDB_URL
+//     ? new Sequelize(process.env.JAWSDB_URL)
