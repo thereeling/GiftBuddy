bfec46272d6b3f89654e07a95a8f100c1bb6aa16
Nick Giuliani <nicholas.giuliani94@gmail.com>
2021-11-05 01:54:02 -0400

    removed host local host from connection

--- config/connection.js
+++ config/connection.js
@@ -5,9 +5,8 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL
     ? new Sequelize(process.env.JAWSDB_URL)
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhost',
         dialect: 'mysql',
         port: process.env.PORT
     });
 
-    module.exports = sequelize;
+module.exports = sequelize;
